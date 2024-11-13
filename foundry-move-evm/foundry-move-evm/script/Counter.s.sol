914c316529e23a55efb0051812d40fae48840cf4
kooded <120389732+kooded@users.noreply.github.com>
2024-11-12 19:00:32 -0800

    Update Counter.s.sol

--- foundry-move-evm/foundry-move-evm/script/Counter.s.sol
+++ foundry-move-evm/foundry-move-evm/script/Counter.s.sol
@@ -6,19 +6,19 @@ import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     Counter public counter;
-    uint256 public deployerPrivateKey = vm.envUint("73a263f2e41315980b7c4e5961945c54ab474828483228852286925f5cc96d7d"); 
+    
+    // Get private key securely from environment variables (use this in the environment setup)
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+    function setUp() public {}
 
     function run() public {
-        // Start broadcasting with the deployer's private key
-        vm.startBroadcast(73a263f2e41315980b7c4e5961945c54ab474828483228852286925f5cc96d7d);
-
+        vm.startBroadcast(deployerPrivateKey);
+        
         // Deploy the contract
         counter = new Counter();
 
         // Stop broadcasting after deployment
         vm.stopBroadcast();
-    }
-}
-
     }
 }
