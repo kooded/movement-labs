8025fd59a9ecec29c812bddff35b18e806c5cb0b
kooded <120389732+kooded@users.noreply.github.com>
2024-11-13 02:31:25 +0000

    Modified Counter.s.sol script

--- foundry-move-evm/foundry-move-evm/script/Counter.s.sol
+++ foundry-move-evm/foundry-move-evm/script/Counter.s.sol
@@ -1,19 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     Counter public counter;
-
-    function setUp() public {}
+    uint256 public deployerPrivateKey = vm.envUint("73a263f2e41315980b7c4e5961945c54ab474828483228852286925f5cc96d7d"); 
 
     function run() public {
-        vm.startBroadcast();
+        // Start broadcasting with the deployer's private key
+        vm.startBroadcast(73a263f2e41315980b7c4e5961945c54ab474828483228852286925f5cc96d7d);
 
+        // Deploy the contract
         counter = new Counter();
 
+        // Stop broadcasting after deployment
         vm.stopBroadcast();
+    }
+}
+
     }
 }
